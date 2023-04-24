abc4fb41a25a8b7af7df7d252656671dcda148d7
陈玲琳 <chenlinglin@chenlinglindeMacBook-Pro.local>
2023-04-24 19:44:24 -0400

    updated and bug

--- model.py
+++ model.py
@@ -5,42 +5,9 @@ from datetime import datetime
 
 db = SQLAlchemy()
 
-class Sign(db.Model):
-    __tablename__ = 'signs'
 
-    sign_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    image_url = db.Column(db.String(255), nullable=False)
-    description = db.Column(db.String(200))
-    categories = db.relationship('Category', secondary='signcategories', back_populates='signs')
 
-    reviews = db.relationship("Review", back_populates="sign")
-
-    def __repr__(self):
-        return f"<Sign sign_id={self.sign_id} name={self.name}>"
-
-class SignCategory(db.Model):
-    __tablename__ = 'signcategories'
-    signcategory_id = db.Column(db.Integer, primary_key=True)
-    sign = db.Column(db.Integer, db.ForeignKey('signs.sign_id'))
-    category = db.Column(db.Integer, db.ForeignKey('categories.category_id'))
-
-    def __repr__(self):
-        return f'<SignCategory sign_id={self.sign_id} category_id={self.category_id}>'
-
-class Category(db.Model):
-    __tablename__ = 'categories'
-
-    category_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-
-    signs = db.relationship('Sign', secondary='signcategories', back_populates='categories')
-
-    def __repr__(self):
-        return f"<Category category_id={self.category_id} name={self.name}>"
-    
 class User(db.Model):
-    """A user."""
 
     __tablename__ = "users"
 
@@ -48,27 +15,145 @@ class User(db.Model):
     email = db.Column(db.String, unique=True)
     password = db.Column(db.String)
 
-    reviews = db.relationship("Review", back_populates="user")
-
     def __repr__(self):
         return f"<User user_id={self.user_id} email={self.email}>"
-    
-class Review(db.Model):
-    """A model for user reviews of signs."""
 
+
+class Sign(db.Model):
+    __tablename__ = 'signs'
+
+    sign_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    image_url = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(200))
+
+    def __repr__(self):
+        return f"<Sign sign_id={self.sign_id} name={self.name}" \
+               f"image_url={self.image_url} description={self.description}>"
+
+
+class SignCategory(db.Model):
+    __tablename__ = 'signcategories'
+
+    signcategory_id = db.Column(db.Integer, primary_key=True)
+    sign_id = db.Column(db.Integer, db.ForeignKey('signs.sign_id'))
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.category_id'))
+
+    sign = db.relationship('Sign', backref='sign_categories')
+    category = db.relationship('Category', backref='sign_categories')
+
+    def __repr__(self):
+        return f'<SignCategory sign_id={self.sign_id} category_id={self.category_id}>'
+
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+
+    category_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f"<Category category_id={self.category_id} name={self.name}>"
+
+
+class Review(db.Model):
     __tablename__ = "reviews"
 
     review_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     sign_id = db.Column(db.Integer, db.ForeignKey('signs.sign_id'))
-    comment_text = db.Column(db.String(200))
+    comment_text = db.Column(db.String(200), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    
-    user = db.relationship("User", back_populates="reviews")
-    sign = db.relationship("Sign", back_populates="reviews")
+
+    user = db.relationship("User", backref="reviews")
+    sign = db.relationship("Sign", backref="reviews")
 
     def __repr__(self):
-        return f"<Review review_id={self.review_id} comment_text={self.comment_text}>"
+        return f"<Review review_id={self.review_id} comment_text={self.comment_text} created_at={self.created_at}>"
+
+
+class Question(db.Model):
+    __tablename__ = "questions"
+
+    question_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quizzes.quiz_id'))
+    question_text = db.Column(db.String(200), nullable=False)
+    option1 = db.Column(db.String(50), nullable=False)
+    option2 = db.Column(db.String(50), nullable=False)
+    option3 = db.Column(db.String(50), nullable=False)
+    answer = db.Column(db.String(1), nullable=False)
+    sign_id = db.Column(db.Integer, db.ForeignKey('signs.sign_id'))
+
+    sign = db.relationship("Sign", backref="signs", uselist=False)
+
+    def __repr__(self):
+        return f"<Question question_id={self.question_id} question_text={self.question_text}"\
+               f"option1={self.option1} option2={self.option2} option3={self.option3} answer={self.answer}>"
+
+
+class Quiz(db.Model):
+    __tablename__ = "quizzes"
+
+    quiz_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    quizname = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f"<Quiz quiz_id={self.quiz_id} quizname={self.quizname}>"
+
+
+class UserQuizScore(db.Model):
+    __tablename__ = "userquizscores"
+
+    userquiz_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quizzes.quiz_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    score = db.Column(db.Integer, default=0)
+    time = db.Column(db.DateTime, default=datetime.utcnow)
+
+    useranswers = db.relationship("UserAnswer", backref="useranswers")
+    quiz = db.relationship("Quiz", backref="quizzes", uselist=False)
+
+    def __repr__(self):
+        return f"<UserQuizScore userquiz_id={self.userquiz_id} quiz_id={self.quiz_id}" \
+               f"user_id={self.user_id} score={self.score} time={self.time}>"
+
+
+class UserAnswer(db.Model):
+    __tablename__ = "useranswers"
+
+    useranswer_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    userquiz_id = db.Column(db.Integer, db.ForeignKey('userquizscores.userquiz_id'))
+    quiz_id = db.Column(db.Integer)
+    question_id = db.Column(db.Integer, db.ForeignKey('questions.question_id'))
+    user_answer = db.Column(db.String(1))
+
+    question = db.relationship("Question", backref=db.backref("questions", order_by=question_id), uselist=False)
+
+    def __repr__(self):
+        return f"<UserAnswer useranswer_id={self.useranswer_id} userquiz_id={self.userquiz_id}" \
+               f"quiz_id={self.quiz_id} question_id={self.question_id} user_answer={self.user_answer}>"
+
+
+class JsonResult:
+    def __init__(self, status, message, data=None):
+        self.status = status
+        self.message = message
+        self.data = data
+
+    def to_dict(self):
+        result_dict = {'status': self.status, 'message': self.message}
+        if self.data is not None:
+            result_dict['data'] = self.data
+        return result_dict
+
+    @classmethod
+    def success(cls, message='OK', data=None):
+        return cls(True, message, data).to_dict()
+
+    @classmethod
+    def fail(cls, message='Server Error', data=None):
+        return cls(False, message, data).to_dict()
+
  
 
 #database name is sign_mapping
--- server.py
+++ server.py
@@ -1,54 +1,188 @@
-from flask import Flask, render_template
-from model import connect_to_db, Sign, Category, SignCategory, Review
-import crud
+from flask import Flask, session, redirect, url_for, request, render_template
+from model import connect_to_db, Sign, SignCategory, Review, User, JsonResult, Quiz, Question, UserQuizScore, UserAnswer, db
+from interceptor import login_required
+import json
+import datetime
+
+
+
 
 app = Flask(__name__)
 app.secret_key = "dev"
 
-# Route to render the homepage
+
+#straight to index.html
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if 'user_id' in session:
+        return render_template('home.html')
+    else:
+        return render_template('index.html')
 
-# Route to get all signs sorted by category and render the signs.html template
-@app.route('/signs', methods=['GET'])
-def get_signs():
-    signs_by_category = {}
+#login
+@app.route('/login', methods=['GET'])
+def login_page():
+    return render_template('login.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+    user = User.query.filter_by(email=username, password=password).first()
+    if user:
+        session['user_id'] = user.user_id
+        session['email'] = user.email
+        return JsonResult.success("Login successful")
+    else:
+        return JsonResult.fail("Login failed, please check if your username or password is wrong")
+
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    if 'user_id' in session:
+        session.clear()
+        return JsonResult.success("You are logged out")
+    else:
+        return JsonResult.fail("You are not logged in")
+
+#register
+@app.before_request
+def set_session_lifetime():
+    session.permanent = True
+    app.permanent_session_lifetime = datetime.timedelta(minutes=5)
+
+
+@app.route('/register', methods=['GET'])
+def register_page():
+    return render_template('register.html')
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    user = User()
+    user.email = request.form['username']
+    user.password = request.form['password']
+    db.session.add(user)
+    db.session.commit()
+    if user in db.session:
+        return JsonResult.success('Register successfully, going to the login page soon')
+    else:
+        return JsonResult.fail('Registration failed')
+    
+#home
+@app.route('/home', methods=['GET'])
+@login_required
+def sign_page():
     sign_categories = SignCategory.query.all()
+    unique_signs = set()
+    unique_categories = set()
+    nodes = []
+    links = []
 
     for sign_category in sign_categories:
-        category_id = sign_category.category_id
-        sign_id = sign_category.sign_id
+        sign = sign_category.sign
+        category = sign_category.category
 
-        if category_id not in signs_by_category:
-            signs_by_category[category_id] = []
+        if sign not in unique_signs:
+            nodes.append({'id': 's' + str(sign.sign_id), 'name': sign.name, 'category': 0})
+            unique_signs.add(sign)
 
-        sign = Sign.query.filter_by(sign_id=sign_id).first()
-        signs_by_category[category_id].append(sign)
+        if category not in unique_categories:
+            nodes.append({'id': 'c' + str(category.category_id), 'name': category.name, 'category': 1})
+            unique_categories.add(category)
 
-    return render_template('signs.html', signs_by_category=signs_by_category)
+        links.append({'source': 's' + str(sign.sign_id), 'target': 'c' + str(category.category_id)})
 
-# Route to get all categories and render the categories.html template
-@app.route('/categories', methods=['GET'])
-def get_categories():
-    categories = Category.query.all()
-    return render_template('categories.html', categories=categories)
+    nodedata = json.dumps(nodes)
+    linkdata = json.dumps(links)
+    return render_template('home.html', nodedata=nodedata, linkdata=linkdata)
 
-@app.route("/users")
-def all_users():
-    """View all users."""
+#sign
+# @app.route('/sign/<sign_id>', methods=['GET'])
+# @login_required
+# def sign_page(sign_id):
+#     asl = Sign.query.get(sign_id)
+#     return render_template('sign.html', asl=asl)
 
-    users = crud.get_users()
 
-    return render_template("all_users.html", users=users)
+@app.route('/sign/<sign_id>', methods=['POST'])
+def save_review(sign_id):
+    review = Review()
+    review.comment_text = request.form['review']
+    review.user_id = session['user_id']
+    review.sign_id = sign_id
+    db.session.add(review)
+    db.session.commit()
+    if review in db.session:
+        return JsonResult.success('Comment successfully, will refresh this page soon')
+    else:
+        return JsonResult.fail('Comment failed')
 
-@app.route("/users/<user_id>")
-def show_user(user_id):
-    """Show details on a particular user."""
 
-    user = crud.get_user_by_id(user_id)
+#quiz
+@app.route('/quizzes', methods=['GET'])
+@login_required
+def quizzes_page():
+    quizzes = Quiz.query.all()
+    return render_template('quizzes.html', quizzes=quizzes)
 
-    return render_template("user_details.html", user=user)
+
+# @app.route('/quiz/<quiz_id>', methods=['GET'])
+# @login_required
+# def sign_page(quiz_id):
+#     questions = Question.query.filter_by(quiz_id=quiz_id).all()
+#     quizzes = Quiz.query.get(quiz_id)
+#     return render_template('quiz.html', questions=questions, quizzes=quizzes)
+
+
+@app.route('/quiz/<quiz_id>', methods=['POST'])
+@login_required
+def save_quiz_score(quiz_id):
+    userquizscore = UserQuizScore()
+    userquizscore.user_id = session['user_id']
+    userquizscore.quiz_id = quiz_id
+    userquizscore.score = request.form.get('score')
+    db.session.add(userquizscore)
+    db.session.commit()
+
+    useranswers = json.loads(request.form.get('answers'))
+    for useranswer in useranswers:
+        useranswer['userquiz_id'] = userquizscore.userquiz_id
+        useranswer['quiz_id'] = quiz_id
+    db.session.bulk_insert_mappings(UserAnswer, useranswers)
+    db.session.commit()
+
+    if userquizscore in db.session:
+        return JsonResult.success('Submitted successfully, going to the score page soon')
+    else:
+        return JsonResult.fail('Submission failure')
+
+#score
+@app.route('/scores', methods=['GET'])
+@login_required
+def scores_page():
+    userquizscores = UserQuizScore.query.filter_by(user_id=session['user_id']).all()
+    return render_template('scores.html', userquizscores=userquizscores)
+
+
+@app.route('/answer/<userquiz_id>', methods=['GET'])
+@login_required
+def answer_page(userquiz_id):
+    userquizscore = UserQuizScore.query.get(userquiz_id)
+    return render_template('answer.html', userquizscore=userquizscore)
+
+#pay
+@app.route('/price')
+def price_page():
+    return render_template('price.html')
+
+
+@app.route('/pay')
+def pay_success_page():
+    return render_template('pay_success.html')
+
 
 if __name__ == "__main__":
     connect_to_db(app)
